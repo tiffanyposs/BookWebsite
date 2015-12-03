237c0671b5c1ee5cbdf47b1198dd1303e92e7539
tiffanyposs <tiffanyposs@gmail.com>
2015-12-03 00:12:18 -0500

    simple styling

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+
+ 
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  	res.send(index)
+})
+
+
+app.listen(8081)
+
+console.log("Alex loves port 8081")
+
+// exports = module.exports = app;
