c7c8a2c07543cfdfab93e4515b2d01067783f518
tiffanyposs <tiffanyposs@gmail.com>
2015-12-05 17:14:55 -0500

    style updates and animations

---
+++ public/js/index.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+
+var animatedScroll = false;
+
+	$(window).scroll(function(){
+	    var position = $(window).scrollTop()
+	    console.log(position)
+	    if (!animatedScroll || position < 50) {
+	        animatedScroll = true;
+	        if  (position < 50){
+
+	          $( "header").removeClass('shownav');
+	    			$( "header" ).stop().animate({
+	    			   "padding-top": '50px',
+	    			}, 100, "linear", function(){
+	    				animatedScroll = false;
+	    			});
+
+	    			$( ".info-row > div" ).stop().animate({
+	    	  	  		"padding": "35px 0",
+	    	    	}, 200, "linear", function(){ animatedScroll = false;})
+
+
+	        } else {
+	        $('header').addClass('shownav')
+		  		$( "header" ).stop().animate({
+			   	 	"padding-top": '10px',
+			  	}, 100, "linear", function(){
+			  		animatedScroll = false; 
+			  	})
+
+			  	$( ".info-row > div" ).stop().animate({
+	    	  	  "padding": "80px 0",
+	    	    }, 200, "linear", function(){ animatedScroll = false;})
+
+
+	        }
+	    }
+	});
+
+
+
+})
